20ca0b1d5c7af714fe7e8a406ee94ef62abcae0b
devardo <devardostubbs@hotmail.com>
2024-04-29 01:41:22 -0400

    added s3 location to providers.py

--- Providers.py
+++ Providers.py
@@ -1,10 +1,10 @@
 Providers =[
-    {"Name": "Cobb County Water System", "Category": "Water"},
-    {"Name": "Greystone Power", "Category": "Power"},
-    {"Name": "Austell Gas", "Category": "Gas"},
-    {"Name": "xfinity", "Category": "Internet"},
-    {"Name": "Xfinity Mobile", "Category": "Mobile"},
-    {"Name": "ADT", "Category": "Security"},
-    {"Name": "THE DIAMOND DISPOSAL, INC", "Category": "Waste"},
+    {"Name": "Cobb County Water System", "Category": "Water", "Location": "Water/"},
+    {"Name": "Greystone Power", "Category": "Power", "Location": "Greystone/"},
+    {"Name": "Austell Gas", "Category": "Gas", "Location": "Gas/"},
+    {"Name": "xfinity", "Category": "Internet", "Location": "Xfinity/"},
+    {"Name": "Xfinity Mobile", "Category": "Mobile", "Location": "Mobile/"},
+    {"Name": "ADT", "Category": "Security", "Location": "Security/"},
+    {"Name": "THE DIAMOND DISPOSAL, INC", "Category": "Waste", "Location": "Waste/"},
    ]
 
